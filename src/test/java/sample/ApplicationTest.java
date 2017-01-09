103e77364fb1f9111ab0d586b0cf8ac56aa868fb
zhanhb <zhanhb88@gmail.com>
2017-01-10 01:29:26 +0800

    Upgrade to platform Athens SR2

--- src/test/java/sample/ApplicationTest.java
+++ src/test/java/sample/ApplicationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 import org.springframework.web.context.WebApplicationContext;
@@ -22,6 +23,7 @@ public class ApplicationTest {
   public void test() throws Exception {
     MockMvc mvc = webAppContextSetup(context).build();
     mvc.perform(get("/"))
+            .andDo(print())
             .andExpect(status().is2xxSuccessful());
 
   }
